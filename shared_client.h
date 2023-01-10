b77b56782f862dfbc7bd7bb192e8a25bff9bc7e9
NikkieDev <dev.nikkie@gmail.com>
2023-01-10 12:46:29 +0100

    Commit

--- shared_client.h
+++
@@ -1,8 +0,0 @@
-#ifndef INDLL_H
-#define INDLL_H
-    #ifdef EXPORTING_DLL
-        extern __declspec(dllexport) void connectToServer(int port, std::string ipAddr, std::string msg);
-    #else
-        extern __declspec(dllimport) void connectToServer(int port, std::string ipAddr, std::string msg);
-    #endif
-#endif
